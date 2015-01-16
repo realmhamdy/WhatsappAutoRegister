b07a650a1b65d7a8d38d3f27c3b7790e4881c2d7
Mohammed Hamdy <mohammed.hamdy777@gmail.com>
2015-01-16 02:04:26 +0200

    Added -o cli argument

--- main.py
+++ main.py
@@ -28,7 +28,7 @@ def make_registration_requests(numbers_file, wav_dir="/var/spool/asterisk/monito
             token = get_token()
             account_id = get_account_id(country_code+phone)
             open("/root/textvoice/success.txt", 'wt').write(country_code+phone + 
-                "," + regcode + "," + account_id + "\n")
+                "," + token + "," + account_id + "\n")
             break
     # sleep between requests
     sleep(request_sleep)
@@ -36,6 +36,7 @@ def make_registration_requests(numbers_file, wav_dir="/var/spool/asterisk/monito
 if __name__ == "__main__":
   parser = ArgumentParser()
   parser.add_argument("-f", "--numbers_file", help="the file that contains numbers to query")
+  parser.add_argument("-o", "--output")
   parser.add_argument("--wav_dir", default="/var/spool/asterisk/monitor/")
   parser.add_argument("--wav_wait", type=int, default=210, help="time to wait for wav files to be downloaded (secs)")
   parser.add_argument("--req_sleep", type=int, default=1, help="time to sleep between whatsapp requests")
